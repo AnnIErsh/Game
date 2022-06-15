a620341faa4694dd45d1ce428f64dfcafc1b06c2
Anna <annaersh1994@gmail.com>
2022-06-16 00:39:05 +0300

    Initial Commit

---
+++ Game/GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameApp.swift
+//  Game
+//
+//  Created by Anna Ershova on 16.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
